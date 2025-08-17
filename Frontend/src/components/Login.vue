62a06da87352c29982328202598f12ffa34f9ace
Yuvraj IITM 2815 <22f3002815@ds.study.iitm.ac.in>
2025-08-17 22:13:49 +0530

    fixed cors

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -69,7 +69,7 @@ export default {
   methods: {
     async handleSubmit() {
       try {
-        const response = await fetch("http://localhost:5000/auth/login", {
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/auth/login`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
